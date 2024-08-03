e06060f8dcb9694a77a61ece6ed5f4e8c57d5d5f
Rohit <2020010006264@gndu.ac.in>
2024-08-03 23:49:14 +0530

    map mock data

--- zomato/src/components/Body.jsx
+++ zomato/src/components/Body.jsx
@@ -1,17 +1,27 @@
 /* eslint-disable react/jsx-key */
 import Card from "./Card";
-// import { restruantList } from "../utils/mockData";
-import { actualList } from "../utils/actualData";
+import { restruantList } from "../utils/mockData";
+// import { actualList } from "../utils/actualData";
 
 const Body = () => {
     return (
         <div className="container">
             <h3 className="my-3">Best Food in Amritsar</h3>
             <div className="row my-3">
+
+                {/***************** MOCK DATA *************************/}
                 {/* <Card restruant={restruantList[0]} />
                 <Card restruant={restruantList[1]} />
                 <Card restruant={restruantList[2]} /> */}
 
+                {
+                    restruantList.map((obj) => {
+                        return <Card restruant={obj} key={obj.key} />
+                    })
+                }
+
+
+                {/*********** SPREAD OPERATOR *****************/}
                 {/* <Card {...restruantList[0]} />
                 <Card {...restruantList[1]} />
                 <Card {...restruantList[2]} /> */}
@@ -22,16 +32,18 @@ const Body = () => {
                     })
                 } */}
 
+
+                {/***************** ACTUAL DATA *************************/}
                 {/* <Card restruant={actualList[0]} />
                 <Card restruant={actualList[1]} />
                 <Card restruant={actualList[2]} />
                 <Card restruant={actualList[3]} /> */}
 
-                {
+                {/* {
                     actualList.map((obj) => {
                         return <Card restruant={obj} key={obj.info.resId} />
                     })
-                }
+                } */}
 
             </div>
         </div>
--- zomato/src/components/Card.jsx
+++ zomato/src/components/Card.jsx
@@ -1,23 +1,24 @@
 /* eslint-disable react/prop-types */
 
-// const Card = (props) => {
-//     console.log(props);
-//     const { name, img, cuizines, stars, price, loc } = props.restruant;
-//     return (
-//         <>
-//             <div className="card col-md-4">
-//                 <img src={img} className="img-fluid" style={{ width: "100%", height: "auto", aspectRatio: "4/3" }} alt="..." />
-//                 <div className="card-body">
-//                     <h5 className="card-title">{name}</h5>
-//                     <h6>{stars}</h6>
-//                     <h6>{cuizines}</h6>
-//                     <p>{loc}</p>
-//                     <p> &#8377;{price} for two</p>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
+/* MOCK DATA */
+const Card = (props) => {
+    console.log(props);
+    const { name, img, cuizines, stars, price, loc } = props.restruant;
+    return (
+        <>
+            <div className="card col-md-4">
+                <img src={img} className="img-fluid" style={{ width: "100%", height: "auto", aspectRatio: "4/3" }} alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <h6>{stars}</h6>
+                    <h6>{cuizines}</h6>
+                    <p>{loc}</p>
+                    <p> &#8377;{price} for two</p>
+                </div>
+            </div>
+        </>
+    )
+}
 
 // const Card = ({ restruant }) => {
 //     const { name, img, cuizines, stars, price, loc } = restruant;
@@ -37,6 +38,7 @@
 //     )
 // }
 
+/* SPREAD OPERATOR */
 // const Card = ({ name, img, cuizines, stars, price, loc }) => {
 //     return (
 //         <>
@@ -55,23 +57,23 @@
 // }
 
 /* ACTUAL DATA */
-const Card = (props) => {
-    console.log(props.restruant.info);
-    const { name, cft, rating, image, locality } = props.restruant.info;
-    return (
-        <>
-            <div className="card col-md-4">
-                <img src={image.url} className="img-fluid" style={{ width: "100%", height: "auto", aspectRatio: "4/3" }} alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <h6>{rating.rating_text}</h6>
-                    <p>{locality.name}</p>
-                    <p>{cft.text}</p>
-                </div>
-            </div>
-        </>
-    )
-}
+// const Card = (props) => {
+//     console.log(props.restruant.info);
+//     const { name, cft, rating, image, locality } = props.restruant.info;
+//     return (
+//         <>
+//             <div className="card col-md-4">
+//                 <img src={image.url} className="img-fluid" style={{ width: "100%", height: "auto", aspectRatio: "4/3" }} alt="..." />
+//                 <div className="card-body">
+//                     <h5 className="card-title">{name}</h5>
+//                     <h6>{rating.rating_text}</h6>
+//                     <p>{locality.name}</p>
+//                     <p>{cft.text}</p>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
 
 
 export default Card;
